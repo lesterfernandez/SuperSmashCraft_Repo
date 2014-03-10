ea9e8e92fe90098fef7140cf4b9e793e6cb6e377
Lester <warlord0315@gmail.com>
2014-03-09 21:08:56 -0400

    Fixed some bugs

--- src/me/thewarlord315/supersmashcraft/Arena.java
+++ src/me/thewarlord315/supersmashcraft/Arena.java
@@ -7,7 +7,6 @@ import me.thewarlord315.supersmashcraft.Arena.ArenaState;
 import me.thewarlord315.supersmashcraft.MessageManager.MessageType;
 
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.configuration.ConfigurationSection;
@@ -18,199 +17,197 @@ public class Arena {
     
 	public enum ArenaState { DISABLED, WAITING, COUNTING_DOWN, STARTED; }
 
-    private int id, taskID, numPlayers, currentPlayers = 0;
-    protected ArenaState state = ArenaState.DISABLED;
-    private ArrayList<PlayerData> data;
-    private ArrayList<Sign> signs;
-    private Location spawnPoint;
-    
-    protected Arena(int id) {
-            this.id = id;
-            this.data = new ArrayList<PlayerData>();
-            this.numPlayers = SettingsManager.getArenas().get("arenas." + id + ".numPlayers");
-            
-            ConfigurationSection s = SettingsManager.getArenas().get("arenas." + id + ".spawn");
-            spawnPoint = LocationUtil.locationFromConfig(s, true);
-            
-            state = ArenaState.WAITING;
-            
-            this.signs = SignManager.getSigns(this);
-    }
-    
-    public int getID() {
-            return id;
-    }
-    
-    public ArenaState getState() {
-            return state;
-    }
-    
-    public int getCurrentPlayers() {
-            return currentPlayers;
-    }
-    
-    public void addPlayer(Player p) {
-            if (currentPlayers >= numPlayers) {
-                    MessageManager.getInstance().msg(p, MessageType.BAD, "There are too many players.");
-                    return;
-            }
-            
-            if (spawnPoint == null) {
-                    MessageManager.getInstance().msg(p, MessageType.BAD, "Spawn point not yet set.");
-                    return;
-            }
-            
-            data.add(new PlayerData(p));
-            
-            p.getInventory().clear();
-            p.setGameMode(GameMode.SURVIVAL);
-            MessageManager.getInstance().msg(p, MessageType.INFO, "Please choose a charater! do /ssc (Character)");
-            
-            p.teleport(spawnPoint);
-            
-            currentPlayers++;
-            
-            for (Sign s : signs) {
-                    s.setLine(2, currentPlayers + "");
-            }
-    }
-    
-    public void removePlayer(Player p) {
-            PlayerData d = getPlayerData(p);
-            d.restorePlayer();
-            data.remove(d);
-            
-            currentPlayers--;
-            
-            for (Sign s : signs) {
-                    s.setLine(2, currentPlayers + "");
-                    s.update();
-            }
-            
-            if (currentPlayers == 1) {
-                    stop(data.get(0).getPlayer());
-            } else if (currentPlayers == 0) {
-        stop();
-    }
-    }
-    
-    public void start() {
-            this.state = ArenaState.COUNTING_DOWN;
-            for (Sign s : signs) {
-                    s.setLine(3, getState().toString());
-                    s.update();
-            }
-            
-            final Countdown c = new Countdown(30, "Game starting in %t seconds!", this, 30, 20, 10, 5, 4, 3, 2, 1);
-            this.taskID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(SuperSmashCraft.getPlugin(), new Runnable() {
-                    public void run() {
-                            if (!c.isDone()) {
-                                    c.run();
-                                    state = ArenaState.STARTED;
-                                    for (Sign s : signs) {
-                                            s.setLine(3, getState().toString());
-                                            s.update();
-                                    }
-                            }
-                            else Bukkit.getServer().getScheduler().cancelTask(taskID);
-                    }
-            }, 0, 20);
-    }
-    
-    public void stop(Player winner) {
-            if (winner != null) MessageManager.getInstance().broadcast(MessageType.GOOD, winner.getName() + " has won arena " + id + "!");
-            
-            for (PlayerData pd : data) pd.restorePlayer();
+	private int id, taskID, numPlayers, currentPlayers = 0;
+	protected ArenaState state = ArenaState.DISABLED;
+	private ArrayList<PlayerData> data;
+	private ArrayList<Sign> signs;
+	private Location spawnPoint;
 
-    this.state = ArenaState.WAITING;
+	protected Arena(int id) {
+		this.id = id;
+		this.data = new ArrayList<PlayerData>();
+		this.numPlayers = SettingsManager.getArenas().get("arenas." + id + ".numPlayers");
+
+		ConfigurationSection s = SettingsManager.getArenas().get("arenas." + id + ".spawn");
+		spawnPoint = LocationUtil.locationFromConfig(s, true);
+
+		state = ArenaState.WAITING;
+
+		this.signs = SignManager.getSigns(this);
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public ArenaState getState() {
+		return state;
+	}
+
+	public int getCurrentPlayers() {
+		return currentPlayers;
+	}
+
+	public void addPlayer(Player p) {
+		if (currentPlayers >= numPlayers) {
+			MessageManager.getInstance().msg(p, MessageType.BAD, "There are too many players.");
+			return;
+		}
+
+		if (spawnPoint == null) {
+			MessageManager.getInstance().msg(p, MessageType.BAD, "Spawn point not yet set.");
+			return;
+		}
+
+		data.add(new PlayerData(p));
+
+		p.getInventory().clear();
+
+		p.teleport(spawnPoint);
+
+		currentPlayers++;
+
+		for (Sign s : signs) {
+			s.setLine(2, currentPlayers + "");
+		}
+	}
+
+	public void removePlayer(Player p) {
+		PlayerData d = getPlayerData(p);
+		d.restorePlayer();
+		data.remove(d);
+
+		currentPlayers--;
+
+		for (Sign s : signs) {
+			s.setLine(2, currentPlayers + "");
+			s.update();
+		}
+
+		if (currentPlayers == 1) {
+			stop(data.get(0).getPlayer());
+		} else if (currentPlayers == 0) {
+            stop();
+        }
+	}
+
+	public void start() {
+		this.state = ArenaState.COUNTING_DOWN;
+		for (Sign s : signs) {
+			s.setLine(3, getState().toString());
+			s.update();
+		}
+
+		final Countdown c = new Countdown(30, "Game starting in %t seconds!", this, 30, 20, 10, 5, 4, 3, 2, 1);
+		this.taskID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(SuperSmashCraft.getPlugin(), new Runnable() {
+			public void run() {
+				if (!c.isDone()) {
+					c.run();
+					state = ArenaState.STARTED;
+					for (Sign s : signs) {
+						s.setLine(3, getState().toString());
+						s.update();
+					}
+				}
+				else Bukkit.getServer().getScheduler().cancelTask(taskID);
+			}
+		}, 0, 20);
+	}
+
+	public void stop(Player winner) {
+		if (winner != null) MessageManager.getInstance().broadcast(MessageType.GOOD, winner.getName() + " has won arena " + id + "!");
+
+		for (PlayerData pd : data) pd.restorePlayer();
+
+        this.state = ArenaState.WAITING;
+	}
+
+    public void stop() {
+        MessageManager.getInstance().broadcast(MessageType.GOOD, "The arena " + id + " has ended!");
+
+        for (PlayerData pd : data) pd.restorePlayer();
+
+        this.state = ArenaState.WAITING;
     }
 
-public void stop() {
-    MessageManager.getInstance().broadcast(MessageType.GOOD, "The arena " + id + " has ended!");
+	public boolean containsPlayer(Player p) {
+		return getPlayerData(p) != null;
+	}
 
-    for (PlayerData pd : data) pd.restorePlayer();
+	private PlayerData getPlayerData(Player p) {
+		for (PlayerData d : data) {
+			if (d.isForPlayer(p)) return d;
+		}
+		return null;
+	}
 
-    this.state = ArenaState.WAITING;
-}
-    
-    public boolean containsPlayer(Player p) {
-            return getPlayerData(p) != null;
-    }
-    
-    private PlayerData getPlayerData(Player p) {
-            for (PlayerData d : data) {
-                    if (d.isForPlayer(p)) return d;
-            }
-            return null;
-    }
-    
-    protected void sendMessage(MessageType type, String... messages) {
-            for (PlayerData d : data) MessageManager.getInstance().msg(d.getPlayer(), type, messages);
-    }
+	protected void sendMessage(MessageType type, String... messages) {
+		for (PlayerData d : data) MessageManager.getInstance().msg(d.getPlayer(), type, messages);
+	}
 }
 
 class PlayerData {
 
-    private String playerName;
-    private ItemStack[] contents, armorContents;
-    private Location location;
-    
-    protected PlayerData(Player p) {
-            this.playerName = p.getName();
-            this.contents = p.getInventory().getContents();
-            this.armorContents = p.getInventory().getArmorContents();
-            this.location = p.getLocation();
-    }
-    
-    protected Player getPlayer() {
-            return Bukkit.getServer().getPlayer(playerName);
-    }
-    
-    protected void restorePlayer() {
-            Player p = Bukkit.getServer().getPlayer(playerName);
-            
-            p.getInventory().setContents(contents);
-            p.getInventory().setArmorContents(armorContents);
-            p.teleport(location);
-    }
-    
-    protected boolean isForPlayer(Player p) {
-            return playerName.equalsIgnoreCase(p.getName());
-    }
+	private String playerName;
+	private ItemStack[] contents, armorContents;
+	private Location location;
+
+	protected PlayerData(Player p) {
+		this.playerName = p.getName();
+		this.contents = p.getInventory().getContents();
+		this.armorContents = p.getInventory().getArmorContents();
+		this.location = p.getLocation();
+	}
+
+	protected Player getPlayer() {
+		return Bukkit.getServer().getPlayer(playerName);
+	}
+
+	protected void restorePlayer() {
+		Player p = Bukkit.getServer().getPlayer(playerName);
+
+		p.getInventory().setContents(contents);
+		p.getInventory().setArmorContents(armorContents);
+		p.teleport(location);
+	}
+
+	protected boolean isForPlayer(Player p) {
+		return playerName.equalsIgnoreCase(p.getName());
+	}
 }
 
 class Countdown implements Runnable {
-    
-    private boolean isDone = false;
-    private int timer;
-    private String msg;
-    private Arena a;
-    private ArrayList<Integer> countingNums;
-    
-    public Countdown(int start, String msg, Arena a, int... countingNums) {
-            this.timer = start;
-            this.msg = msg;
-            this.a = a;
-            this.countingNums = new ArrayList<Integer>();
-            for (int i : countingNums) this.countingNums.add(i);
-    }
-    
-    public void run() {
-            if (timer == 0) {
-                    a.sendMessage(MessageType.GOOD, "The game has begun!");
-                    a.state = ArenaState.STARTED;
-                    isDone = true;
-                    return;
-            }
-            
-            if (countingNums.contains(timer)) {
-                    a.sendMessage(MessageType.INFO, msg.replaceAll("%t", timer + ""));
-            }
-            
-            timer--;
-    }
-    
-    public boolean isDone() {
-            return isDone;
-    }
+
+	private boolean isDone = false;
+	private int timer;
+	private String msg;
+	private Arena a;
+	private ArrayList<Integer> countingNums;
+
+	public Countdown(int start, String msg, Arena a, int... countingNums) {
+		this.timer = start;
+		this.msg = msg;
+		this.a = a;
+		this.countingNums = new ArrayList<Integer>();
+		for (int i : countingNums) this.countingNums.add(i);
+	}
+
+	public void run() {
+		if (timer == 0) {
+			a.sendMessage(MessageType.GOOD, "The game has begun!");
+			a.state = ArenaState.STARTED;
+			isDone = true;
+			return;
+		}
+
+		if (countingNums.contains(timer)) {
+			a.sendMessage(MessageType.INFO, msg.replaceAll("%t", timer + ""));
+		}
+
+		timer--;
+	}
+
+	public boolean isDone() {
+		return isDone;
+	}
 }
--- src/me/thewarlord315/supersmashcraft/ArenaManager.java
+++ src/me/thewarlord315/supersmashcraft/ArenaManager.java
@@ -7,41 +7,42 @@ import org.bukkit.entity.Player;
 
 public class ArenaManager {
 
-	private ArenaManager() { }
-    
-    private static ArenaManager instance = new ArenaManager();
-    
-    public static ArenaManager getInstance() {
-            return instance;
-    }
-    
-    private ArrayList<Arena> arenas = new ArrayList<Arena>();
-    
-    public void setupArenas() {
-            if (SettingsManager.getArenas().<ConfigurationSection>get("arenas") == null) SettingsManager.getArenas().createConfigurationSection("arenas");
 
-    arenas.clear();
-            
-            for (String key : SettingsManager.getArenas().<ConfigurationSection>get("arenas").getKeys(false)) {
-                    arenas.add(new Arena(Integer.parseInt(key)));
-            }
-    }
-    
-    public ArrayList<Arena> getArenas() {
-            return arenas;
-    }
-    
-    public Arena getArena(int id) {
-            for (Arena a : arenas) {
-                    if (a.getID() == id) return a;
-            }
-            return null;
-    }
-    
-    public Arena getArena(Player p) {
-            for (Arena a : arenas) {
-                    if (a.containsPlayer(p)) return a;
-            }
-            return null;
-    }
+	private ArenaManager() { }
+
+	private static ArenaManager instance = new ArenaManager();
+
+	public static ArenaManager getInstance() {
+		return instance;
+	}
+
+	private ArrayList<Arena> arenas = new ArrayList<Arena>();
+
+	public void setupArenas() {
+		if (SettingsManager.getArenas().<ConfigurationSection>get("arenas") == null) SettingsManager.getArenas().createConfigurationSection("arenas");
+
+        arenas.clear();
+
+		for (String key : SettingsManager.getArenas().<ConfigurationSection>get("arenas").getKeys(false)) {
+			arenas.add(new Arena(Integer.parseInt(key)));
+		}
+	}
+
+	public ArrayList<Arena> getArenas() {
+		return arenas;
+	}
+
+	public Arena getArena(int id) {
+		for (Arena a : arenas) {
+			if (a.getID() == id) return a;
+		}
+		return null;
+	}
+
+	public Arena getArena(Player p) {
+		for (Arena a : arenas) {
+			if (a.containsPlayer(p)) return a;
+		}
+		return null;
+	}
 }
